0dcc7f48d87dead9e524b5303c2b4e3f2a33b000
Sak <107257050+Suxcl@users.noreply.github.com>
2025-02-12 23:49:11 +0100

    fixes

--- Weather_Station/Debug/Core/Src/LoRa.d
+++
@@ -1,2 +0,0 @@
-Core/Src/LoRa.o: ../Core/Src/LoRa.c ../Core/Inc/LoRa.h
-../Core/Inc/LoRa.h:
--- Weather_Station/Debug/Drivers/LoRa/SX1278.d
+++
@@ -1,4 +0,0 @@
-Drivers/LoRa/SX1278.o: ../Drivers/LoRa/SX1278.c ../Drivers/LoRa/SX1278.h \
- ../Drivers/LoRa/SX1278_hw.h
-../Drivers/LoRa/SX1278.h:
-../Drivers/LoRa/SX1278_hw.h:
